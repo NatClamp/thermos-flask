6448f6750a9952bf530e300a10693a42f79e0e55
NatClamp <nat.clamp@gmail.com>
2019-02-24 18:30:24 +0000

    Using template inheritance to build basic page

--- thermos/app.py
+++ thermos/app.py
@@ -2,21 +2,18 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-class User:
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
-
-    def initials(self):
-        return "{}. {}.".format(self.firstname[0], self.lastname[0])
-
 @app.route('/')
 def index():
-    return render_template('index.html', text='It would make me happy if you were happy.', title='How do you do?', user=User("Natalie", "Clamp"))
+    return render_template('index.html')
 
 @app.route('/add')
 def add():
     return render_template('add.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run()
